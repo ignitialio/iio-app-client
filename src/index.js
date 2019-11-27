a520b797c73db42da25309e13acafde89c58be03
Victor Andritoiu <devandrito@gmail.com>
2019-11-27 18:40:00 +0100

    expose encoders

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Services from './services'
 import Modules from './modules'
 import Utils from './utils'
 import I18n from './i18n'
+import Encoders from './encoders'
 
 export {
   Services,
   Modules,
   Utils,
-  I18n
+  I18n,
+  Encoders
 }
